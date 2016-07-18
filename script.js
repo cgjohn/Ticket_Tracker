6a39d3258afec35f43dd514ce5acf0dadde33dca
cgjohn <cgjohn@umich.edu>
2016-07-18 21:03:38 +0200

    what I think is a working login

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var app = angular.module('trackerApp', ['ngRoute', 'firebase']); 
+var app = angular.module('ticketApp', ['ngRoute', 'firebase']); 
 
 app.run(["$rootScope", "$location", function($rootScope, $location) {
   $rootScope.$on("$routeChangeError", function(event, next, previous, error) {
@@ -18,10 +18,35 @@ app.config(function($routeProvider) {
 		
     })
 
-	$routeProvider.when('/signup', {
-		controller: 'SignupCtrl',
-		templateUrl: 'templates/signup.html',
+	$routeProvider.when('/login', {
+		controller: 'LoginCtrl',
+		templateUrl: 'templates/login.html',
 	})
 
+});
+
+app.controller('HomeCtrl', function($scope, $firebaseAuth, $firebaseObject, $window) {
+   
+});
+
+app.controller('LoginCtrl', function($scope, $firebaseAuth, $firebaseObject, $window) {
+    $scope.authObj = $firebaseAuth();
+	
+	$scope.login = function(){
+		console.log($scope.email);
+		console.log($scope.password);
+
+		$scope.authObj.$signInWithEmailAndPassword($scope.email, $scope.password)
+		.then(function(firebaseUser) {
+		  console.log("Signed in as:", firebaseUser.uid);
+		  $scope.firebaseUser1 = firebaseUser;
+		  console.log($scope.firebaseUser1);
+		  $window.location.href = '#/';
+		}).catch(function(error) {
+		  console.error("Authentication failed:", error);
+		  $scope.errorMessage = error.message;
+		  
+		});
+	}
 });
 
