948ac4b55df1c360340ff23bb89500c33766532c
cgjohn <cgjohn@umich.edu>
2016-07-18 20:49:30 +0200

    added angular.js and angular-route files

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-script.js
+var app = angular.module('trackerApp', ['ngRoute', 'firebase']); 
+
+app.run(["$rootScope", "$location", function($rootScope, $location) {
+  $rootScope.$on("$routeChangeError", function(event, next, previous, error) {
+    // We can catch the error thrown when the $requireSignIn promise is rejected
+    // and redirect the user back to the home page
+    if (error === "AUTH_REQUIRED") {
+      $location.path("/login");
+    }
+  });
+}]);
+
+
+app.config(function($routeProvider) {
+	$routeProvider.when('/', {
+		controller: 'HomeCtrl',
+		templateUrl: 'templates/home.html',
+		
+    })
+
+	$routeProvider.when('/signup', {
+		controller: 'SignupCtrl',
+		templateUrl: 'templates/signup.html',
+	})
+
+});
+
