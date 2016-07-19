f02fc2f46a6d23cc058c20d07e87e993d7620927
cgjohn <cgjohn@umich.edu>
2016-07-20 01:59:46 +0200

    added images and some styling to the home html

--- script.js
+++ script.js
@@ -88,6 +88,19 @@ app.controller('LoginCtrl', function($scope, $firebaseAuth, $firebaseObject, $wi
 
 app.controller('MainCtrl', function($scope, $firebaseAuth, $firebaseObject, $window) {
     $scope.authObj = $firebaseAuth();
+
+ 
+    var svg = dimple.newSvg("body", 800, 600);
+    var data = [
+      { "Word":"Hello", "Awesomeness":2000 },
+      { "Word":"World", "Awesomeness":3000 }
+    ];
+    var chart = new dimple.chart(svg, data);
+    chart.addCategoryAxis("x", "Word");
+    chart.addMeasureAxis("y", "Awesomeness");
+    chart.addSeries(null, dimple.plot.bar);
+    chart.draw();
+
 	
 });
 
