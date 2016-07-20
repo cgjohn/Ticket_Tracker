6babe25bbe4203a9171bcf2d58e3717d4a00384f
Courtney <courtneypasco23@yahoo.com>
2016-07-20 12:09:05 +0200

    Added Update Methods

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ app.config(function($routeProvider) {
 
 });
 
-app.controller('HomeCtrl', function($scope, $firebaseAuth, $firebaseObject, $window) {
+app.controller('HomeCtrl', function($scope, $http, $firebaseAuth, $firebaseObject, $firebaseArray, $window) {
    $scope.authObj = $firebaseAuth();
 	
     $scope.errorMessage = "";
@@ -52,6 +52,64 @@ app.controller('HomeCtrl', function($scope, $firebaseAuth, $firebaseObject, $win
 				$scope.errorMessage = error.message;
 			});
 	}
+
+	$scope.update = function() {
+		$scope.eventIDs = []
+		$scope.events = $firebaseArray(firebase.database().ref().child('events'));
+		$scope.events.$loaded().then(function(){
+	        angular.forEach($scope.events, function(eventID) {
+	        	$scope.eventIDs.push(eventID);
+	      	});
+	      	updateListing($scope.eventIDs);
+	    });
+	};
+
+	var updateListing = function(eventIDs) {
+		if (eventIDs.length === 0) {
+			return;
+		}
+
+		var eventID = eventIDs.splice(0, 1)[0];
+
+	    if (Date.parse(eventID.dateOfEvent) > Date.now()) {
+        	$http({
+				url: "https://api.stubhub.com/search/inventory/v1",
+				method: "GET",
+				headers: {'Authorization': "Bearer iqzOnUo98P2SGazfIz0oQ227Wnoa"},
+				params: {
+					'eventid': eventID.$id,
+					'pricingsummary': true
+				}
+			}).then(function(response) {
+				$scope.listings = $firebaseObject(firebase.database().ref().child('listings').child(eventID.$id));
+				$scope.listings.$loaded().then(function() {
+					console.log($scope.listings);
+					console.log(response.data.pricingSummary.averageTicketPrice)
+					$scope.listings[Date.now()] = response.data.pricingSummary.averageTicketPrice;
+					$scope.listings.$save();
+					updateListing(eventIDs);
+				});
+			});
+	    } else {
+	    	updateListing(eventIDs);
+	    };
+	}
+
+	// var getListingPrices = function(id) {
+	// 	$http({
+	// 		url: "https://api.stubhub.com/search/inventory/v1",
+	// 		method: "GET",
+	// 		headers: {'Authorization': "Bearer iqzOnUo98P2SGazfIz0oQ227Wnoa"},
+	// 		params: {
+	// 			'eventid': id,
+	// 			'pricingsummary': true
+	// 		}
+	// 	}).then(function(response) {
+	// 		$scope.listings[Date.now()] = response.data.pricingSummary.averageTicketPrice;
+	// 		console.log($scope.listings);
+	// 		$scope.listings.$save();
+	// 	});
+	// }
 });
 
 app.controller('LoginCtrl', function($scope, $firebaseAuth, $firebaseObject, $window) {
