565a97ab0d1bde335b78a06cfeb9a22e9ed6e30b
Courtney <courtneypasco23@yahoo.com>
2016-07-21 10:25:42 +0200

    Current and Historical Dataset

--- script.js
+++ script.js
@@ -129,120 +129,337 @@ app.controller('LoginCtrl', function($scope, $firebaseAuth, $firebaseObject, $wi
 	}
 });
 
-app.controller('MainCtrl', function($scope, $firebaseAuth, $firebaseObject, $firebaseArray, $window) {
+app.controller('MainCtrl', function($scope, $q, $firebaseAuth, $firebaseObject, $firebaseArray, $window) {
     $scope.authObj = $firebaseAuth();
 
-   	$scope.getData = function (eventID) {
-		console.log("I make it here");
-		$scope.dataset0 = [];
-		$scope.dates = [];
-		$scope.prices = [];
+    $scope.searchEvent = function() {
+    	$scope.historicalDataset = [];
+    	$scope.currentDataset = [];
+    	$scope.events = $firebaseArray(firebase.database().ref().child('events'));
+    	$scope.events.$loaded().then(function() {
+    		var allPromises = [];
+			angular.forEach($scope.events, function(myEvent) {
+				var search = $scope.searchText.toLowerCase();
+				var eventName = myEvent.name.toLowerCase();
+				if (eventName.indexOf(search) > -1) {
+					if (Date.parse(myEvent.dateOfEvent) < Date.now()) {
+						$scope.oldlistings = $firebaseObject(firebase.database().ref().child('listings').child(myEvent.$id));
+						allPromises.push($scope.oldlistings.$loaded().then(function() {
+							angular.forEach($scope.oldlistings, function(price, date) {
+								$scope.historicalDataset.push({
+									date: new Date(parseInt(date)),
+									value: price
+								})
+							});
+						}));
+					} else {
+						$scope.curlistings = $firebaseObject(firebase.database().ref().child('listings').child(myEvent.$id));
+						$scope.curlistings.$loaded().then(function() {
+							angular.forEach($scope.curlistings, function(price, date) {
+								$scope.currentDataset.push({
+									date: new Date(parseInt(date)),
+									value: price
+								})
+							});
+						});
+					}
+				}
+			})
+			$q.all(allPromises).then(function(results) {
+				var width = 500;
+				var height = 250;    
+
+				// Create the SVG 'canvas'
+				var svg = d3.select("p")
+				    .append("svg")
+				    .attr("viewBox", "0 0 " + width + " " + height)
+
+
+				// get the data
+				var dataset = $scope.currentDataset;
+				var oldDataset = $scope.historicalDataset;
+
+				console.log(dataset, " <--- dataset");
+				console.log(oldDataset, " <--- old event dataset");
+				// Define the padding around the graph
+				var padding = 50;
+
+				// Set the scales
+				var minDate = d3.min(dataset, function(d) { return d.date; });
+				minDate.setDate(minDate.getDate() - 1);
+
+				var maxDate = d3.max(dataset, function(d) { return d.date; });
+
+				var xScale = d3.time.scale()
+				    .domain([minDate, maxDate])
+				    .range([padding, width - padding]);
+
+				var yScale = d3.scale.linear()
+				    .domain([d3.min(dataset, function(d) { return d.value - 100; }), d3.max(dataset, function(d) { return d.value + 100; })])
+				    .range([height - padding, padding]);
+
+				// x-axis
+				var format = d3.time.format("%d %b");
+				var xAxis = d3.svg.axis()
+				    .scale(xScale)
+				    .orient("bottom")
+				    .tickFormat(format)
+				    .ticks(d3.time.days, 1);
+
+				svg.append("g")
+				    .attr("class", "axis x-axis")
+				    .attr("transform", "translate(0," + (height - padding) + ")")
+				    .call(xAxis);
+
+				// y-axis
+				var yAxis = d3.svg.axis()
+				    .scale(yScale)
+				    .orient("left")
+				    .tickFormat(function (d) { return d; })
+				    .tickSize(5, 5, 0)
+				    .ticks(5); // set rough # of ticks
+
+				svg.append("g")
+				    .attr("class", "axis y-axis")
+				    .attr("transform", "translate(" + padding + ",0)")
+				    .call(yAxis);
+
+				// draw line graph
+				var line = d3.svg.line()
+				    .x(function(d) { 
+				        return xScale(d.date); 
+				    })
+				    .y(function(d) { 
+				        return yScale(d.value); 
+				    });
+
+				svg.append("svg:path").attr("d", line(dataset));
+
+				// plot circles
+				svg.selectAll("circle")
+				    .data(dataset)
+				    .enter()
+				    .append("circle")
+				    .attr("class", "data-point")
+				    .attr("cx", function(d) {
+				        return xScale(d.date);
+				    })
+				    .attr("cy", function(d) {
+				        return yScale(d.value);
+				    })
+				    .attr("r", 5)
+				    .on("mouseover", function(d,i) {
+				    	console.log("hi");
+					    d3.select(this).append("text")
+					        .text(function(d) {return d.value;})
+					        .attr("xScale", xScale(d.date))
+					        .attr("yScale", yScale(d.value)); 
+					    });
+			});
+		})
+	}
+	// }.then(function () {
+
+
+	// 		var width = 500;
+	// 		var height = 250;    
+
+	// 		// Create the SVG 'canvas'
+	// 		var svg = d3.select("p")
+	// 		    .append("svg")
+	// 		    .attr("viewBox", "0 0 " + width + " " + height)
+
+
+	// 		// get the data
+	// 		var dataset = $scope.currentDataset;
+
+	// 		console.log(dataset, " <--- dataset");
+	// 		// Define the padding around the graph
+	// 		var padding = 50;
+
+	// 		// Set the scales
+	// 		var minDate = d3.min(dataset, function(d) { return d.date; });
+	// 		minDate.setDate(minDate.getDate() - 1);
+
+	// 		var maxDate = d3.max(dataset, function(d) { return d.date; });
+
+	// 		var xScale = d3.time.scale()
+	// 		    .domain([minDate, maxDate])
+	// 		    .range([padding, width - padding]);
+
+	// 		var yScale = d3.scale.linear()
+	// 		    .domain([d3.min(dataset, function(d) { return d.value - 100; }), d3.max(dataset, function(d) { return d.value + 100; })])
+	// 		    .range([height - padding, padding]);
+
+	// 		// x-axis
+	// 		var format = d3.time.format("%d %b");
+	// 		var xAxis = d3.svg.axis()
+	// 		    .scale(xScale)
+	// 		    .orient("bottom")
+	// 		    .tickFormat(format)
+	// 		    .ticks(d3.time.days, 1);
+
+	// 		svg.append("g")
+	// 		    .attr("class", "axis x-axis")
+	// 		    .attr("transform", "translate(0," + (height - padding) + ")")
+	// 		    .call(xAxis);
+
+	// 		// y-axis
+	// 		var yAxis = d3.svg.axis()
+	// 		    .scale(yScale)
+	// 		    .orient("left")
+	// 		    .tickFormat(function (d) { return d; })
+	// 		    .tickSize(5, 5, 0)
+	// 		    .ticks(5); // set rough # of ticks
+
+	// 		svg.append("g")
+	// 		    .attr("class", "axis y-axis")
+	// 		    .attr("transform", "translate(" + padding + ",0)")
+	// 		    .call(yAxis);
+
+	// 		// draw line graph
+	// 		var line = d3.svg.line()
+	// 		    .x(function(d) { 
+	// 		        return xScale(d.date); 
+	// 		    })
+	// 		    .y(function(d) { 
+	// 		        return yScale(d.value); 
+	// 		    });
+
+	// 		svg.append("svg:path").attr("d", line(dataset));
+
+	// 		// plot circles
+	// 		svg.selectAll("circle")
+	// 		    .data(dataset)
+	// 		    .enter()
+	// 		    .append("circle")
+	// 		    .attr("class", "data-point")
+	// 		    .attr("cx", function(d) {
+	// 		        return xScale(d.date);
+	// 		    })
+	// 		    .attr("cy", function(d) {
+	// 		        return yScale(d.value);
+	// 		    })
+	// 		    .attr("r", 5)
+	// 		    .on("mouseover", function(d,i) {
+	// 		    	console.log("hi");
+	// 			    d3.select(this).append("text")
+	// 			        .text(function(d) {return d.value;})
+	// 			        .attr("xScale", xScale(d.date))
+	// 			        .attr("yScale", yScale(d.value)); 
+	// 			    });
+	// 		});
+
+   	$scope.getData = function (dataset, eventID) {
+   		console.log(eventID);
 		$scope.listings = $firebaseObject(firebase.database().ref().child('listings').child(eventID));
 		$scope.listings.$loaded().then(function() {
-			console.log($scope.listings);
 			angular.forEach($scope.listings, function(price, date) {
-				$scope.dataset0.push({
+				dataset.push({
 					date: new Date(parseInt(date)),
 					value: price
 				})
-				$scope.dates.push(new Date(parseInt(date)));
-				$scope.prices.push(price);
 			});
-			console.log($scope.dataset0, "<-- dataset0");
-		}).then(function(){
 
-			var width = 500;
-			var height = 250;    
+			console.log("Historical", $scope.historicalDataset);
+			console.log("Current", $scope.currentDataset);
+			//console.log(dataset, "<-- dataset0");
+		});
+	}
+	 //.then(function(){
 
-			// Create the SVG 'canvas'
-			var svg = d3.select("p")
-			    .append("svg")
-			    .attr("viewBox", "0 0 " + width + " " + height)
+		// 	var width = 500;
+		// 	var height = 250;    
+
+		// 	// Create the SVG 'canvas'
+		// 	var svg = d3.select("p")
+		// 	    .append("svg")
+		// 	    .attr("viewBox", "0 0 " + width + " " + height)
 
 
-			// get the data
-			var dataset = $scope.dataset0;
+		// 	// get the data
+		// 	var dataset = $scope.dataset0;
 
-			console.log(dataset, " <--- dataset");
-			// Define the padding around the graph
-			var padding = 50;
+		// 	console.log(dataset, " <--- dataset");
+		// 	// Define the padding around the graph
+		// 	var padding = 50;
 
-			// Set the scales
-			var minDate = d3.min(dataset, function(d) { return d.date; });
-			minDate.setDate(minDate.getDate() - 1);
+		// 	// Set the scales
+		// 	var minDate = d3.min(dataset, function(d) { return d.date; });
+		// 	minDate.setDate(minDate.getDate() - 1);
 
-			var maxDate = d3.max(dataset, function(d) { return d.date; });
+		// 	var maxDate = d3.max(dataset, function(d) { return d.date; });
 
-			var xScale = d3.time.scale()
-			    .domain([minDate, maxDate])
-			    .range([padding, width - padding]);
+		// 	var xScale = d3.time.scale()
+		// 	    .domain([minDate, maxDate])
+		// 	    .range([padding, width - padding]);
 
-			var yScale = d3.scale.linear()
-			    .domain([d3.min(dataset, function(d) { return d.value - 100; }), d3.max(dataset, function(d) { return d.value + 100; })])
-			    .range([height - padding, padding]);
+		// 	var yScale = d3.scale.linear()
+		// 	    .domain([d3.min(dataset, function(d) { return d.value - 100; }), d3.max(dataset, function(d) { return d.value + 100; })])
+		// 	    .range([height - padding, padding]);
 
-			// x-axis
-			var format = d3.time.format("%d %b");
-			var xAxis = d3.svg.axis()
-			    .scale(xScale)
-			    .orient("bottom")
-			    .tickFormat(format)
-			    .ticks(d3.time.days, 1);
+		// 	// x-axis
+		// 	var format = d3.time.format("%d %b");
+		// 	var xAxis = d3.svg.axis()
+		// 	    .scale(xScale)
+		// 	    .orient("bottom")
+		// 	    .tickFormat(format)
+		// 	    .ticks(d3.time.days, 1);
 
-			svg.append("g")
-			    .attr("class", "axis x-axis")
-			    .attr("transform", "translate(0," + (height - padding) + ")")
-			    .call(xAxis);
+		// 	svg.append("g")
+		// 	    .attr("class", "axis x-axis")
+		// 	    .attr("transform", "translate(0," + (height - padding) + ")")
+		// 	    .call(xAxis);
 
-			// y-axis
-			var yAxis = d3.svg.axis()
-			    .scale(yScale)
-			    .orient("left")
-			    .tickFormat(function (d) { return d; })
-			    .tickSize(5, 5, 0)
-			    .ticks(5); // set rough # of ticks
+		// 	// y-axis
+		// 	var yAxis = d3.svg.axis()
+		// 	    .scale(yScale)
+		// 	    .orient("left")
+		// 	    .tickFormat(function (d) { return d; })
+		// 	    .tickSize(5, 5, 0)
+		// 	    .ticks(5); // set rough # of ticks
 
-			svg.append("g")
-			    .attr("class", "axis y-axis")
-			    .attr("transform", "translate(" + padding + ",0)")
-			    .call(yAxis);
+		// 	svg.append("g")
+		// 	    .attr("class", "axis y-axis")
+		// 	    .attr("transform", "translate(" + padding + ",0)")
+		// 	    .call(yAxis);
 
-			// draw line graph
-			var line = d3.svg.line()
-			    .x(function(d) { 
-			        return xScale(d.date); 
-			    })
-			    .y(function(d) { 
-			        return yScale(d.value); 
-			    });
+		// 	// draw line graph
+		// 	var line = d3.svg.line()
+		// 	    .x(function(d) { 
+		// 	        return xScale(d.date); 
+		// 	    })
+		// 	    .y(function(d) { 
+		// 	        return yScale(d.value); 
+		// 	    });
 
-			svg.append("svg:path").attr("d", line(dataset));
+		// 	svg.append("svg:path").attr("d", line(dataset));
 
-			// plot circles
-			svg.selectAll("circle")
-			    .data(dataset)
-			    .enter()
-			    .append("circle")
-			    .attr("class", "data-point")
-			    .attr("cx", function(d) {
-			        return xScale(d.date);
-			    })
-			    .attr("cy", function(d) {
-			        return yScale(d.value);
-			    })
-			    .attr("r", 5)
-			    .on("mouseover", function(d,i) {
-			    	console.log("hi");
-				    d3.select(this).append("text")
-				        .text(function(d) {return d.value;})
-				        .attr("xScale", xScale(d.date))
-				        .attr("yScale", yScale(d.value)); 
-				    });
+		// 	// plot circles
+		// 	svg.selectAll("circle")
+		// 	    .data(dataset)
+		// 	    .enter()
+		// 	    .append("circle")
+		// 	    .attr("class", "data-point")
+		// 	    .attr("cx", function(d) {
+		// 	        return xScale(d.date);
+		// 	    })
+		// 	    .attr("cy", function(d) {
+		// 	        return yScale(d.value);
+		// 	    })
+		// 	    .attr("r", 5)
+		// 	    .on("mouseover", function(d,i) {
+		// 	    	console.log("hi");
+		// 		    d3.select(this).append("text")
+		// 		        .text(function(d) {return d.value;})
+		// 		        .attr("xScale", xScale(d.date))
+		// 		        .attr("yScale", yScale(d.value)); 
+		// 		    });
 
-			});
+		// 	});
 		
 	
-		}
+		// }
 	
 });
 
